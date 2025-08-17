4e1ef72aa00e54034ca81083432fa45a3a2cd6e5
Evan Alvarez <evanfalvarez@tutamail.com>
2025-08-16 21:32:25 -0500

    im restarting

--- src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-   import * as d3 from 'd3';
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
