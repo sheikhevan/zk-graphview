ec5330c0dc0741254bd628d63760db3361bb995c
Evan Alvarez <evanfalvarez@tutamail.com>
2025-08-22 14:11:35 -0500

    eh im kinda lazy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,30 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
+	import * as d3 from 'd3';
 
 	export let data;
-	let graph = data.graph;
+	let graphJson = data.graph;
 
-	onMount(() => {
-		if (!graph && browser) {
+	onMount(async () => {
+		if (!graphJson && browser) {
 			const stored = localStorage.getItem('zk-graph');
 			if (stored) {
-				graph = JSON.parse(stored);
+				graphJson = JSON.parse(stored);
 			}
 		}
 	});
 
-	$: if (graph) {
-		console.log(graph);
+	$: if (graphJson) {
+		const nodes = graphJson.notes || [];
+		const links = graphJson.links || [];
+		createGraph(nodes, links);
+	}
+
+	function createGraph(nodes: any[], links: any[]) {
+		if (!graphJson) return;
+		console.log(nodes);
+		console.log(links);
 	}
 
 	function handleUpload(event: Event) {
@@ -24,9 +33,9 @@
 			const reader = new FileReader();
 			reader.onload = (e) => {
 				const content = e.target?.result;
-				graph = JSON.parse(content as string);
+				graphJson = JSON.parse(content as string);
 				if (browser) {
-					window.localStorage.setItem('zk-graph', JSON.stringify(graph));
+					window.localStorage.setItem('zk-graph', JSON.stringify(graphJson));
 				}
 			};
 			reader.readAsText(json);
